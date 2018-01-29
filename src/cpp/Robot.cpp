fbd13c7fc4e8b66c3e533ba42f9b6a291f59a8a5
Eric <eric99b@gmail.com>
2018-01-29 16:52:08 -0500

    New Autonomous workflow on the dashboard created

--- src/cpp/Robot.cpp
+++ src/cpp/Robot.cpp
@@ -28,7 +28,6 @@
 #include "CommandBase.h"
 #include "Commands/TeleopDefault.h"
 #include "Commands/Auto/Default.h"
-#include "Commands/ResetGyro.h"
 #include "Commands/DoNothing.h"
 #include "Commands/Auto/PrioritizeSwitchLeft.h"
 #include "Commands/Auto/PrioritizeSwitchRight.h"
@@ -44,14 +43,16 @@ class Robot : public TimedRobot {
 private:
 	SendableChooser<Command*>AutonomousChooser;
 	SendableChooser<Command*> TeleopChooser;
+	SendableChooser<int> StartingPositionChooser;
+	SendableChooser<int> PriorityChooser;
 	unique_ptr<Command> AutoMode;
 	unique_ptr<Command> TeleMode;
-	Command * resetGyro;
+
+	Command * Teleop = new TeleopDefault();
 
 public:
 	void RobotInit() override {
         CommandBase::Init();
-		resetGyro = new ResetGyro();
 		CommandBase::drivetrain->Calibrate();
 	}
 
@@ -62,6 +63,13 @@ public:
 		TeleopChooser.AddDefault("Default Driver", new TeleopDefault());
 		AutonomousChooser.AddDefault("Default Auto", new AutoDefault());
 
+		StartingPositionChooser.AddDefault("LEFT", 0);
+		StartingPositionChooser.AddObject("CENTER", 1);
+		StartingPositionChooser.AddObject("RIGHT", 2);
+
+		PriorityChooser.AddDefault("SWITCH", 0);
+		PriorityChooser.AddObject("SCALE", 1);
+
 		AutonomousChooser.AddObject("[LEFT] Switch", new AutoPrioritizeSwitchLeft());
 		AutonomousChooser.AddObject("[RIGHT] Switch", new AutoPrioritizeSwitchRight());
 		AutonomousChooser.AddObject("[LEFT] Scale", new AutoPrioritizeScaleLeft());
@@ -70,10 +78,11 @@ public:
 		CommandBase::drivetrain->ResetGyro();
 		CommandBase::drivetrain->SetRefAngle(CommandBase::drivetrain->GetGyro());
 
-		SmartDashboard::PutData("Teleop Modes", &TeleopChooser);
+		//SmartDashboard::PutData("Teleop Modes", &TeleopChooser);
 		SmartDashboard::PutData("Auto Modes", &AutonomousChooser);
 
-		SmartDashboard::PutData("Reset Gyro", new ResetGyro());
+		SmartDashboard::PutData("Starting Position", &StartingPositionChooser);
+		SmartDashboard::PutData("Auto Priority", &PriorityChooser);
 
         SmartDashboard::PutNumber("Gyro", 0);
 		SmartDashboard::PutNumber("Error", 0);
@@ -86,10 +95,58 @@ public:
 
 	}
 	void AutonomousInit() override {
-		AutoMode.release();
+		/* AutoMode.release();
 		AutoMode.reset(AutonomousChooser.GetSelected());
 		if (AutoMode != nullptr) {
 			AutoMode->Start();
+		}*/
+
+		int StartPos = StartingPositionChooser.GetSelected();
+		int Priority = PriorityChooser.GetSelected();
+
+		switch (StartPos) {
+			default: {
+
+			}
+			case 0: {
+				switch (Priority) {
+					default: {
+
+					}
+					case 0: {
+
+					}
+					case 1: {
+
+					}
+				}
+			}
+			case 1: {
+				switch (Priority) {
+					default: {
+
+					}
+					case 0: {
+
+					}
+					case 1: {
+
+					}
+				}
+			}
+			case 2: {
+				switch (Priority) {
+					default: {
+
+					}
+					case 0: {
+
+					}
+					case 1: {
+
+					}
+				}
+			}
 		}
 	}
 
@@ -98,19 +155,23 @@ public:
 	}
 
 	void TeleopInit() override {
-		TeleMode.release();
+		/*TeleMode.release();
 		TeleMode.reset(TeleopChooser.GetSelected());
 		if (TeleMode != nullptr) {
 			TeleMode->Start();
-		}
+		}*/
+		Teleop->Start();
 	}
 
 	void TeleopPeriodic() override {
 		Scheduler::GetInstance()->Run();
 	}
 
-	void TestPeriodic() override {
+	void TestInit() override {
 
+	}
+
+	void TestPeriodic() override {
 	}
 
 };
